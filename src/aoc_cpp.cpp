ffd9a20722964d4df225aabfb704125fa2bc6065
Alex Do <alexxdo2004@gmail.com>
2025-11-29 13:59:53 +0000

    feat(cli): add structure for command line

--- src/aoc_cpp.cpp
+++ src/aoc_cpp.cpp
@@ -1,1 +1,27 @@
-auto main() -> int { return 0; }
+#include <CLI/CLI.hpp>
+
+#include "lib/cli/app.hpp"
+#include "lib/cli/dotenv.hpp"
+
+auto main(int argc, char **argv) -> int {
+  auto load_env_success = aoc::cli::load_dotenv();
+  if (!load_env_success) {
+    std::println(stderr, "{}", load_env_success.error());
+    return -1;
+  }
+
+  auto cookie = std::getenv("AOC_SESSION_COOKIE");
+  auto year = std::getenv("AOC_YEAR");
+  auto app = CLI::App();
+  auto parsedYear = atoi(year);
+  if (parsedYear < 1900 ||
+      parsedYear > std::numeric_limits<std::uint16_t>::max()) {
+    std::println(stderr, "{} was not a valid year for the variable AOC_YEAR",
+                 year);
+  }
+
+  aoc::cli::build_cli(app, cookie, static_cast<uint16_t>(parsedYear));
+
+  CLI11_PARSE(app, argc, argv);
+  return 0;
+}
